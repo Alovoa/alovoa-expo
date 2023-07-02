809e3ccae458553796c0bd312fd4b18fd64149e2
Nho Quy Dinh <nonononoki@gmail.com>
2023-07-02 14:27:44 +0200

    Clean up

--- components/index.ts
+++ components/index.ts
@@ -2,4 +2,3 @@ export { default as CardItem } from "./CardItem";
 export { default as Filters } from "./Filters";
 export { default as Icon } from "./Icon";
 export { default as Message } from "./Message";
-export { default as ProfileItem } from "./ProfileItem";
--- types.ts
+++ types.ts
@@ -21,17 +21,6 @@ export type MessageT = {
   conversation: ConversationDto;
 };
 
-export type ProfileItemT = {
-  age?: string;
-  info1?: string;
-  info2?: string;
-  info3?: string;
-  info4?: string;
-  location?: string;
-  matches: string;
-  name: string;
-};
-
 export type TabBarIconT = {
   focused: boolean;
   iconName: any;
