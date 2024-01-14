6f4c49ed37d65a81ffce70747078981dc14e8f46
Nho Quy Dinh <nonononoki@gmail.com>
2024-01-14 14:44:15 +0100

    Prevent clickjacking

--- script/staging-web/deploy.sh
+++ script/staging-web/deploy.sh
@@ -5,5 +5,5 @@ PORT=10081
 
 cd ../..
 fuser -k $PORT/tcp
-nohup npx serve dist --single -l $PORT --ssl-cert $CERT_PATH --ssl-pass "ssl-key" &
+nohup npx serve dist --single -l $PORT --ssl-cert $CERT_PATH --ssl-pass "ssl-key" -c "../serve.json" &
 
