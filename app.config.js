5c604e3e9bb8b7dcf11b95d643a9262dc0e8353d
Nho Quy Dinh <nonononoki@gmail.com>
2025-04-20 15:14:22 +0200

    Update version

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ module.exports = {
   "expo": {
     "name": "Alovoa",
     "slug": "alovoa-expo",
-    "version": "2.2.1",
+    "version": "2.2.2",
     "scheme": "alovoa",
     "orientation": "portrait",
     "userInterfaceStyle": "automatic",
@@ -96,7 +96,7 @@ module.exports = {
         "android.permission.ACCESS_FINE_LOCATION",
         "android.permission.RECORD_AUDIO"
       ],
-      "versionCode": 39
+      "versionCode": 40
     },
     "web": {
       "favicon": "./assets/favicon.png"
