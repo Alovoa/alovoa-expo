be9cefe00d3b6a26dc326139c62318ece9468b47
Nho Quy Dinh <nonononoki@gmail.com>
2025-10-23 19:07:52 +0200

    Remove deploy scripts

--- scripts/production-web/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ex
-
-CERT_PATH="/opt/alovoa/alovoa.pfx"
-PORT=10081
-
-cd ../..
-fuser -k $PORT/tcp
-yarn serve dist --single -l $PORT --ssl-cert $CERT_PATH --ssl-pass "ssl-key" -c "../serve.json"
