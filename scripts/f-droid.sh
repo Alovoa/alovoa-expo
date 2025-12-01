d048b4010588b0994944ef136912bb2e4acffdc4
Nho Quy Dinh <nonononoki@gmail.com>
2025-12-01 15:04:16 +0100

    Update f-droid.sh

--- scripts/f-droid.sh
+++ scripts/f-droid.sh
@@ -9,6 +9,9 @@ set -ex
 #use older expo-location that can be easily patched
 yarn add expo-location@18.0.7
 
+#remove expo-dev-client since it has non-free deps and only needed in development
+yarn remove expo-dev-client
+
 # apply node_module patches (`rm -rf node_modules && yarn` to reverse)
 yarn patch-package --patch-dir scripts/patches
 
