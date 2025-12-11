55eb44bece3e3215007fd47fedbf6276fbaf2c3f
Nho Quy Dinh <nonononoki@gmail.com>
2025-12-11 18:00:57 +0100

    Delete deploy.sh

--- scripts/staging-web/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CERT_PATH="/opt/alovoa/alovoa.pfx"
-PORT=10082
-
-cd ../..
-fuser -k $PORT/tcp
-yarn serve dist --single -l $PORT --ssl-cert $CERT_PATH --ssl-pass "ssl-key" -c "../serve.json"
